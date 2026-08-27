6b7934292b15781099f338bf2da2a778983a9ec1
Suraj <surajbarnwal24@gmail.com>
2026-08-27 22:15:10 +0530

    Add: palindrome-partitioning - submission-3

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-3.java
@@ -0,0 +1,41 @@
+class Solution {
+    List<String> sub;
+    List<List<String>> ans;
+
+    private boolean isPal(String s, int i, int j) {
+        while(i <= j) {
+            if(s.charAt(i++) != s.charAt(j--))
+                return false;
+        }
+
+        return true;
+    }
+
+    private void getAllSubs(int ind, String s) {
+        if(ind == s.length()) {
+            ans.add(new ArrayList<>(sub));
+            return;
+        }
+
+        for(int i=ind;i<s.length();i++) {
+            if(!isPal(s, ind, i))
+                continue;
+            
+            sub.add(s.substring(ind, i+1));
+
+            getAllSubs(i+1, s);
+
+            // backtrack:
+            sub.remove(sub.size()-1);
+        }
+    }
+
+    public List<List<String>> partition(String s) {
+        this.sub = new ArrayList<>();
+        this.ans = new ArrayList<>();
+
+        getAllSubs(0, s);
+
+        return this.ans;
+    }
+}
