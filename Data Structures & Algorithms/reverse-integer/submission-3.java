d96d28071b715edc4670f7b1349b97f2551833c3
Suraj <surajbarnwal24@gmail.com>
2026-09-23 17:02:34 +0530

    Add: reverse-integer - submission-3

---
+++ Data Structures & Algorithms/reverse-integer/submission-3.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int reverse(int x) {
+        if(x >= Integer.MAX_VALUE || x <= Integer.MIN_VALUE) {
+            return 0;
+        }
+
+        if(x < 0) {
+            int val = reverse(Math.abs(x));
+            if(val < 0)
+                return 0;
+            
+            return -1*val;
+        }
+        
+        int num = x;
+        int rev = 0;
+        int k = 0;
+        while(num > 0) {
+            int d = num%10;
+            
+            rev = rev*10 + d;
+            System.out.println("rev: " + rev);
+            if(rev % 10 != d)
+                return 0;
+            
+            k++;
+            num /= 10;
+        }
+
+        return rev;
+    }
+}
