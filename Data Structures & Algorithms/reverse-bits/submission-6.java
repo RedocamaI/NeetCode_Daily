bc1ed15dbcc9c3c34525378519ab791932cb0939
Suraj <surajbarnwal24@gmail.com>
2026-09-25 17:34:31 +0530

    Add: reverse-bits - submission-6

---
+++ Data Structures & Algorithms/reverse-bits/submission-6.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverseBits(int n) {
+        int num = n;
+        int val = 0;
+
+        for(int i=0;i<32;i++) {
+            int setBit = num & 1;
+
+            if(setBit == 1) {
+                val = val | (1 << (31 - i));
+            }
+
+            num >>>= 1;
+        }
+
+        return val;
+    }
+}
